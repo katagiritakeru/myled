ffccf3858f36f3f3c519369be58a9b6ae0557da0
katagiritakeru <s195003@stn.nagaokaut.acjp>
2021-01-16 16:17:20 +0000

    repir

--- myled2.c
+++ myled2.c
@@ -4,25 +4,25 @@
 MODULE_AUTHOR("katagiritakeru";)
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("0.0.1";)
+MODULE_VERSION("0.0.1");
 
 static dev_t dev;
 static int __init init_mod(void) //カーネルモジュールの初期化
 {
-	int retval;
-	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
-	if(retval < 0){
-	        printk(KERN_ERR "alloc_chrdev_region failed \n");
-		        return retval;
-}
+		int retval;
+		retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+		if(retval < 0){
+		printk(KERN_ERR "alloc_chrdev_region failed \n");
+		return retval;
+		}
 
-	printk(KERN_INFO "%s is loaded major \n",__FILE__,MAJOR(dev));
-	return 0;
+		printk(KERN_INFO "%s is loaded major;%d\n",__FILE__,MAJOR(dev));
+		return 0;
 
 static void __exit cleanup_mod(void) //後始末
 {
-	unregister_chrdev_region(dev, 1);        
-	printk(KERN_INFO "%s is unloaded, major:%d\n",__FILE__,MAJOR(dev));
+		unregister_chrdev_region(dev, 1);        
+		printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
 
 module_init(init_mod);     // マクロで関数を登録
