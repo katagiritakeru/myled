3250579f62ebac8b84f3a7f9ab1e31a7ce8c5f69
katagiritakeru <s195003@stn.nagaokaut.acjp>
2021-01-17 04:19:02 +0000

    Add slide nomver9

--- myled.c
+++ myled.c
@@ -2,13 +2,14 @@
 
 static int __init init_mod(void) //カーネルモジュールの初期化
 {
-	printk(KERN_INFO "%s is loaded.")	
-	return 0;
+		printk(KERN_INFO "%s is loaded.\n",__FILE__);	
+				return 0;
 }
 
 static void __exit cleanup_mod(void) //後始末
 {
+		printk(KERN_INFO "%s is unloaded.\n",__FILE__);
 }
 
 module_init(init_mod);     // マクロで関数を登録
-module_exit(clean_mod); 
+module_exit(cleanup_mod); 
